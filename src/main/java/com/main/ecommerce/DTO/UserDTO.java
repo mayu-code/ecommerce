8ef4ff0c9eeb570e121f76bbf2c882aa48d56048
mayu-code <durbulemayu@gmail.com>
2024-12-04 13:02:07 +0530

    mainController done .

--- src/main/java/com/main/ecommerce/DTO/UserDTO.java
+++ src/main/java/com/main/ecommerce/DTO/UserDTO.java
@@ -1,5 +1,24 @@
 package com.main.ecommerce.DTO;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
 public class UserDTO {
     
+    private long id;
+    private String name;
+	private String email;
+	private String mobileNo;
+	private List<Long> myOrders = new ArrayList<>();
+	private List<Long> myCart = new ArrayList<>();
+
 }
--- src/main/java/com/main/ecommerce/controller/MainController.java
+++ src/main/java/com/main/ecommerce/controller/MainController.java
@@ -1,14 +1,63 @@
 package com.main.ecommerce.controller;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.main.ecommerce.entities.Product;
+import com.main.ecommerce.services.impl.ProductServiceImpl;
+import org.springframework.web.bind.annotation.GetMapping;
+
+
 
 @RestController
 @RequestMapping("/Home")
 public class MainController {
 
+    @Autowired
+    ProductServiceImpl productServiceImpl;
 
-    public ResponseEntity
+    
+    public ResponseEntity<List<Product>> allProducts1(){
+        List<Product> products = new ArrayList<>();
+        try{
+            products = productServiceImpl.allProducts();
+            return ResponseEntity.of(Optional.of(products));
+        }
+        catch(Exception e){
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).build();
+        }
+    }
+
+    @GetMapping("getProductByCategery/{categery}")
+    public ResponseEntity<List<Product>> allProductsbyCategery(@PathVariable("categery") String categery){
+        List<Product> products = new ArrayList<>();
+        try{
+            products = productServiceImpl.getProductbyCategery(categery);
+            return ResponseEntity.of(Optional.of(products));
+
+        }catch(Exception e){
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).build();
+        }
+    }
+
+    @GetMapping("getProductBySubCategery/{subcategery}")
+    public ResponseEntity<List<Product>> allProductsbySubCategery(@PathVariable("Subcategery") String subcategery){
+        List<Product> products = new ArrayList<>();
+        try{
+            products = productServiceImpl.getProductbyCategery(subcategery);
+            return ResponseEntity.of(Optional.of(products));
+
+        }catch(Exception e){
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).build();
+        }
+    }
     
 }
