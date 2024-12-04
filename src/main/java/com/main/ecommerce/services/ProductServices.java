3ab02e237c0f2928d648d4b5f418d5aa81d74e20
mayu-code <durbulemayu@gmail.com>
2024-12-04 12:07:11 +0530

    productservice

--- src/main/java/com/main/ecommerce/services/ProductServices.java
+++ src/main/java/com/main/ecommerce/services/ProductServices.java
@@ -1,6 +1,22 @@
 package com.main.ecommerce.services;
 
+import java.util.List;
+
+import com.main.ecommerce.entities.Product;
+
 public interface ProductServices {
+
+    public List<Product> getProductbyCategery(String categery);
+    
+    public List<Product> getProdectbySubCategery(String SubCategery);
+
+    public Product getProductbyId(long id);
+
+    public Product updateProduct(Product product);
+
+    public List<Product> allProducts ();
+
+    public Product AddProduct(Product product);
 
     
 }
