fd7478252f72ce78a4756199e07e26fac494147b
Nilesh3524 <nileshnarule24@gmail.com>
2024-12-04 12:33:09 +0530

    admin interface done

--- src/main/java/com/main/ecommerce/services/AdminServices.java
+++ src/main/java/com/main/ecommerce/services/AdminServices.java
@@ -1,10 +1,14 @@
 package com.main.ecommerce.services;
 
+import java.util.List;
 import java.util.Optional;
 
 import com.main.ecommerce.entities.Admin;
+import com.main.ecommerce.entities.Product;
 
 public interface AdminServices {
 
     Optional<Admin> getAdminById(long id);
+
+    Optional<List<Product>> getAllProductsByAdmin(Admin Admin);
 }
