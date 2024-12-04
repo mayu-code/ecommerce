f0d201a1b67f1dffcb5e8ef281f1eedb680e8b0a
mayu-code <durbulemayu@gmail.com>
2024-12-04 12:25:02 +0530

    product impl

--- src/main/java/com/main/ecommerce/repository/ProductReposotory.java
+++ src/main/java/com/main/ecommerce/repository/ProductReposotory.java
@@ -1,10 +1,14 @@
 package com.main.ecommerce.repository;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.main.ecommerce.entities.Product;
 
 public interface ProductReposotory extends JpaRepository<Product,Long>{
 
+   public List<Product> findByCategory(String category);
+   public List<Product> findBySubcategory(String subcategory);
     
 } 
---
+++ src/main/java/com/main/ecommerce/services/impl/ProductServiceImpl.java
@@ -0,0 +1,51 @@
+package com.main.ecommerce.services.impl;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+
+import com.main.ecommerce.entities.Product;
+import com.main.ecommerce.repository.ProductReposotory;
+import com.main.ecommerce.services.ProductServices;
+
+public class ProductServiceImpl implements ProductServices{
+
+    @Autowired
+    ProductReposotory productReposotory;
+
+    @Override
+    public List<Product> getProductbyCategery(String categery) {
+        List<Product> products = productReposotory.findByCategory(categery);
+        return products;
+    }
+
+    @Override
+    public List<Product> getProdectbySubCategery(String SubCategery) {
+        List<Product> products = productReposotory.findBySubcategory(SubCategery);
+        return products;  
+    }
+
+    @Override
+    public Product getProductbyId(long id) {
+        Product product = productReposotory.findById(id).get();
+        return product;
+    }
+
+    @Override
+    public Product updateProduct(Product product) {
+        return null;
+    }
+
+    @Override
+    public List<Product> allProducts() {
+       List<Product> products = productReposotory.findAll();
+       return products;   
+    }
+
+    @Override
+    public Product AddProduct(Product product) {
+        Product product2 = productReposotory.save(product);
+        return product2;   
+    }
+    
+}
