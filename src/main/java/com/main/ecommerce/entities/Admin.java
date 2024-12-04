27a30cffed0f484e1a8f4b69c78fb9f72459b5d6
Nilesh3524 <nileshnarule24@gmail.com>
2024-12-04 16:25:58 +0530

    admin change

--- src/main/java/com/main/ecommerce/entities/Admin.java
+++ src/main/java/com/main/ecommerce/entities/Admin.java
@@ -8,6 +8,9 @@ import com.fasterxml.jackson.annotation.JsonIgnore;
 import jakarta.persistence.CascadeType;
 import jakarta.persistence.Entity;
 import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
 import jakarta.persistence.OneToMany;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
@@ -21,6 +24,8 @@ import lombok.Setter;
 @AllArgsConstructor
 public class Admin{
     
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
     private long id;
     private String userName;
     private String password;
