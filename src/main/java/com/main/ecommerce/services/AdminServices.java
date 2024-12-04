9b1c67c0e755831ddf04ce6a49f543adde561a88
Nilesh3524 <nileshnarule24@gmail.com>
2024-12-04 12:30:51 +0530

    admin service

--- src/main/java/com/main/ecommerce/services/AdminServices.java
+++ src/main/java/com/main/ecommerce/services/AdminServices.java
@@ -1,6 +1,10 @@
 package com.main.ecommerce.services;
 
+import java.util.Optional;
+
+import com.main.ecommerce.entities.Admin;
+
 public interface AdminServices {
 
-    
+    Optional<Admin> getAdminById(long id);
 }
