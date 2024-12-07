e95448c951e80b8e718fa3a99402b449d97e3377
mayu-code <durbulemayu@gmail.com>
2024-12-07 23:48:38 +0530

    admin controller

--- src/main/java/com/main/ecommerce/controller/AdminController.java
+++ src/main/java/com/main/ecommerce/controller/AdminController.java
@@ -3,6 +3,7 @@ package com.main.ecommerce.controller;
 import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
@@ -15,6 +16,7 @@ import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.main.ecommerce.ResponseEntity.AuthResponse;
 import com.main.ecommerce.entities.Admin;
 import com.main.ecommerce.entities.Product;
 import com.main.ecommerce.entities.User;
@@ -24,75 +26,75 @@ import com.main.ecommerce.services.impl.UserServiceImpl;
 
 @RestController
 @RequestMapping("/admin")
-@CrossOrigin(origins = {"http://localhost:5173/","http://localhost:5174/"})
+@CrossOrigin(origins = { "http://localhost:5173/", "http://localhost:5174/" })
 public class AdminController {
-    
+
     @Autowired
     private ProductServiceImpl productService;
-    
+
     @Autowired
     private AdminServiceImpl adminService;
-    
+
     @Autowired
     private UserServiceImpl userService;
 
     @PostMapping("/addProduct")
-    public ResponseEntity<?> addProduct(@RequestHeader("Authorization") String jwt,@RequestBody Product product){
+    public ResponseEntity<AuthResponse> addProduct(@RequestHeader("Authorization") String jwt,
+            @RequestBody Product product) {
         Admin admin = this.adminService.getAdminByJwt(jwt);
-        this.adminService.addProductWithAdminId(product,admin.getId());
+        this.adminService.addProductWithAdminId(product, admin.getId());
         // this.productService.AddProduct(product);
 
-        return new ResponseEntity<>("Product Added Successfully !!",HttpStatus.CREATED);
+        AuthResponse response = new AuthResponse();
+
+        response.setStatus(HttpStatus.CREATED);
+        response.setMessage("Product Added Successfull");
+
+        return ResponseEntity.of(Optional.of(response));
 
     }
 
-
     @GetMapping("/products")
-    public ResponseEntity<List<Product>> getProducts(@RequestHeader("Authorization") String jwt){
+    public ResponseEntity<List<Product>> getProducts(@RequestHeader("Authorization") String jwt) {
 
         List<Product> products = new ArrayList<>();
 
-        try{
+        try {
             Admin admin = this.adminService.getAdminByJwt(jwt);
             List<Product> productsbyAdmin = this.adminService.getAllProductsByAdmin(admin).get();
-            return new ResponseEntity<>(productsbyAdmin,HttpStatus.OK);
-        }catch(Exception e){
-            return new ResponseEntity<>(products,HttpStatus.OK);
+            return new ResponseEntity<>(productsbyAdmin, HttpStatus.OK);
+        } catch (Exception e) {
+            return new ResponseEntity<>(products, HttpStatus.OK);
         }
 
-
-
     }
-
 
     @PostMapping("/products/update")
-    public ResponseEntity<Product> updateProduct(@RequestHeader("Authorization") String jwt,@RequestBody Product product){
+    public ResponseEntity<Product> updateProduct(@RequestHeader("Authorization") String jwt,
+            @RequestBody Product product) {
         Product updatedProduct = this.productService.AddProduct(product);
-        return new ResponseEntity<>(updatedProduct,HttpStatus.OK);
+        return new ResponseEntity<>(updatedProduct, HttpStatus.OK);
     }
 
-
     @PostMapping("/users/updateUser")
-    public ResponseEntity<?> updateUser(@RequestHeader("Authorization") String jwt,@RequestBody User user){
+    public ResponseEntity<?> updateUser(@RequestHeader("Authorization") String jwt, @RequestBody User user) {
         user.setUpdateDate(LocalDateTime.now());
         this.adminService.updateUser(user);
 
-        return new ResponseEntity<>("User Updated Successfully !",HttpStatus.OK);
+        return new ResponseEntity<>("User Updated Successfully !", HttpStatus.OK);
     }
-
 
     // @GetMapping("/products/ordered")
     // public ResponseEntity<?> getOrdeProducts(){
 
-    //     return ResponseEntity().OK;
+    // return ResponseEntity().OK;
 
     // }
 
     @GetMapping("/allUsers")
-    public ResponseEntity<List<User>> getAllUsers(@RequestHeader("Authorization") String jwt){
+    public ResponseEntity<List<User>> getAllUsers(@RequestHeader("Authorization") String jwt) {
         List<User> allUsers = this.userService.getAllUsers();
         return ResponseEntity.ok(allUsers);
     }
-
 
 }
