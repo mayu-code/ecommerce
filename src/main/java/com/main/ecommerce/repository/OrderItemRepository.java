88bc4272351a06d4c2185a82d2081ac31f663c77
mayu-code <durbulemayu@gmail.com>
2024-12-09 16:38:36 +0530

    sync changes

--- src/main/java/com/main/ecommerce/repository/OrderItemRepository.java
+++ src/main/java/com/main/ecommerce/repository/OrderItemRepository.java
@@ -3,10 +3,7 @@ package com.main.ecommerce.repository;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.main.ecommerce.entities.OrderItem;
-import java.util.List;
 
 public interface OrderItemRepository extends JpaRepository<OrderItem, Long> {
-
-    List<OrderItem> findByUserId(long id);
 
 }
