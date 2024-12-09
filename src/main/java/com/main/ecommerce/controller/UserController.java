cca682ad25aeea0fb72e85e9058fb96de0c2dd30
mayu-code <durbulemayu@gmail.com>
2024-12-09 15:28:13 +0530

    sync

--- src/main/java/com/main/ecommerce/controller/UserController.java
+++ src/main/java/com/main/ecommerce/controller/UserController.java
@@ -4,15 +4,12 @@ import org.springframework.web.bind.annotation.RestController;
 
 import com.main.ecommerce.ResponseEntity.AuthResponse;
 import com.main.ecommerce.ResponseEntity.DataResponse;
-import com.main.ecommerce.entities.Address;
-import com.main.ecommerce.entities.Product;
 import com.main.ecommerce.entities.User;
 import com.main.ecommerce.services.impl.ProductServiceImpl;
 import com.main.ecommerce.services.impl.UserServiceImpl;
 
 import java.time.LocalDateTime;
-import java.util.ArrayList;
-import java.util.List;
+
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -37,69 +34,6 @@ public class UserController {
     @Autowired
     private UserServiceImpl userServiceImpl;
 
-    @GetMapping("/cartproducts")
-    public ResponseEntity<DataResponse> getCartProducts(@RequestHeader("Authorization") String jwt) {
-        User user = userServiceImpl.getUserByJwt(jwt);
-        List<Product> products = new ArrayList<>();
-
-        DataResponse response = new DataResponse();
-
-        try {
-            products = productServiceImpl.getProductsByIds(user.getMyCart());
-            response.setStatus(HttpStatus.OK);
-            response.setMessage("Products fetched Successfully");
-            response.setData(products);
-
-            return ResponseEntity.of(Optional.of(response));
-        } catch (Exception e) {
-            response.setStatus(HttpStatus.INTERNAL_SERVER_ERROR);
-            response.setMessage("Products fetched failed");
-            response.setData(products);
-
-            return ResponseEntity.of(Optional.of(response));
-        }
-    }
-
-    @GetMapping("/orderedproducts")
-    public ResponseEntity<DataResponse> getOrderedProduct(@RequestHeader("Authorization") String jwt) {
-        User user = userServiceImpl.getUserByJwt(jwt);
-        List<Product> products = new ArrayList<>();
-
-        DataResponse response = new DataResponse();
-        try {
-            products = productServiceImpl.getProductsByIds(user.getMyOrders());
-            response.setStatus(HttpStatus.OK);
-            response.setMessage("Products fetched Successfully");
-            response.setData(products);
-
-            return ResponseEntity.of(Optional.of(response));
-        } catch (Exception e) {
-            response.setStatus(HttpStatus.INTERNAL_SERVER_ERROR);
-            response.setMessage("Products fetched failed");
-            response.setData(products);
-
-            return ResponseEntity.of(Optional.of(response));
-        }
-    }
-
-    // @PostMapping("/updateUser")
-    // public ResponseEntity<User> updateUser(@RequestHeader("Authorization") String
-    // jwt ,User user){
-    // User user1 = this.userServiceImpl.getUserByJwt(jwt);
-    // user1.setEmail(user.getEmail());
-    // user1.setAddress(user.getAddress());
-    // user1.setMobileNo(user.getMobileNo());
-    // user1.setName(user.getName());
-    // user1.setPassword(user.getPassword());
-    // user1.setUpdateDate(LocalDateTime.now());
-    // try{
-    // this.userServiceImpl.registerUser(user1);
-    // return ResponseEntity.of(Optional.of(user1));
-    // }
-    // catch(Exception e){
-    // return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).build();
-    // }
-    // }
     @PostMapping("/updateUser")
     public ResponseEntity<DataResponse> updateUser(@RequestHeader("Authorization") String jwt,
             @RequestBody User updatedUser) {
@@ -137,69 +71,6 @@ public class UserController {
         }
     }
 
-    @PostMapping("/addCart/{productId}")
-    public ResponseEntity<DataResponse> AddCardProduct(@RequestHeader("Authorization") String jwt,
-            @PathVariable("productId") long ProductId) {
-
-        User user = this.userServiceImpl.getUserByJwt(jwt);
-
-        DataResponse response = new DataResponse();
-
-        try {
-            userServiceImpl.addCart(user, ProductId);
-            response.setStatus(HttpStatus.OK);
-            response.setMessage("Product added onto the cart succesfully !");
-
-            return ResponseEntity.of(Optional.of(response));
-        } catch (Exception e) {
-            response.setStatus(HttpStatus.INTERNAL_SERVER_ERROR);
-            response.setMessage("Product added onto the cart failed !");
-
-            return ResponseEntity.of(Optional.of(response));
-        }
-    }
-
-    @PostMapping("/removeCart/{productId}")
-    public ResponseEntity<DataResponse> removeCardProduct(@RequestHeader("Authorization") String jwt,
-            @PathVariable("productId") long ProductId) {
-
-        User user = this.userServiceImpl.getUserByJwt(jwt);
-
-        DataResponse response = new DataResponse();
-
-        try {
-            userServiceImpl.removeCart(user, ProductId);
-            response.setStatus(HttpStatus.OK);
-            response.setMessage("Product remove from the cart the cart succesfully ! ");
-
-            return ResponseEntity.ok(response);
-        } catch (Exception e) {
-            response.setStatus(HttpStatus.INTERNAL_SERVER_ERROR);
-            response.setMessage("Product remove from the cart the cart failed ! ");
-            return ResponseEntity.ok(response);
-        }
-    }
-
-    @PostMapping("/ordered/{ProductId}")
-    public ResponseEntity<DataResponse> addOrderedProduct(@RequestHeader("Authorization") String jwt,
-            @PathVariable("ProductId") long ProductId) {
-        User user = this.userServiceImpl.getUserByJwt(jwt);
-
-        DataResponse response = new DataResponse();
-
-        try {
-            userServiceImpl.addOrder(user, ProductId);
-            response.setStatus(HttpStatus.OK);
-            response.setMessage("Product Orederd succesfully ! ");
-
-            return ResponseEntity.ok(response);
-        } catch (Exception e) {
-            response.setStatus(HttpStatus.INTERNAL_SERVER_ERROR);
-            response.setMessage("Product Orederd failed ! ");
-
-            return ResponseEntity.ok(response);
-        }
-    }
 
     @GetMapping("/getUser")
     public ResponseEntity<DataResponse> getUserById(@RequestHeader("Authorization") String jwt) {
@@ -247,5 +118,14 @@ public class UserController {
 
         }
 
+    }
+
+
+    public ResponseEntity<DataResponse> addCartItem(@RequestHeader("Authorization") String jwt,@PathVariable("id") long id,@PathVariable("quantity") int quantity){
+        try{
+            return null;
+        }catch(Exception e){
+            return null;
+        }
     }
 }
