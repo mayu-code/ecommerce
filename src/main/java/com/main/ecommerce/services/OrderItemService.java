a72ca637256f8533db0b42804856836dd87d760a
mayu-code <durbulemayu@gmail.com>
2024-12-09 15:26:54 +0530

    sevice done

--- src/main/java/com/main/ecommerce/services/OrderItemService.java
+++ src/main/java/com/main/ecommerce/services/OrderItemService.java
@@ -1,5 +1,7 @@
 package com.main.ecommerce.services;
 
+import java.util.List;
+
 import com.main.ecommerce.entities.OrderItem;
 import com.main.ecommerce.entities.Product;
 import com.main.ecommerce.entities.User;
@@ -9,4 +11,6 @@ public interface OrderItemService {
     OrderItem addOrderiItem(User user, Product product, int quantity);
 
     void removeOrderItemByOrderItemId(long orderItemId);
+
+    List<OrderItem> getOrderItemsByUserId(long userId);
 }
